96d0a1c39c68f0e9275ee3cbd7a12a68dfae2b20
licler <ute3@yandex.ru>
2021-06-24 12:00:54 +0300

    new animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,65 @@
 $(function(){
+
+// header
+  $(function() {
+    let header = $('.header');
+     
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 1) {
+       header.addClass('header__fixed');
+      } else {
+       header.removeClass('header__fixed');
+      }
+    });
+   });
+
+  
+
+//  let scrollOffset = 0,
+//      previousH = $('.previous').innerHeight(),
+//      header = $('.header');
+     
+
+//   $(window).on('scroll', function(){
+//      scrollOffset = $(this).scrollTop();
+//      if(scrollOffset >=previousH){
+//      header.addClass('header__fixed');
+//      } else {
+//       header.removeClass('header__fixed');
+//      }
+// });
+
+$("[data-scroll]").on("click", function(event) {
+  event.preventDefault();
+
+  var $this = $(this),
+      blockId = $this.data('scroll'),
+      blockOffset = $(blockId).offset().top;
+
+  $("#nav a").removeClass("header__item-link--active");
+  $this.addClass("header__item-link--active");
+
+  $("html, body").animate({
+      scrollTop:  blockOffset
+  }, 500);
+});
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	//  начало счетчик
 	$('.numpage__item-num').each(function () {
         $(this).prop('Counter', 0).animate({
@@ -16,8 +77,9 @@ $(function(){
 
 
 // конец счетчик
- 
 
+
+//  slider
     $('.people__slider').slick({	
         arrows: true,
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/content/back.svg" alt="images"></button>',
@@ -36,30 +98,25 @@ $(function(){
       });
 
    
-
+// footer
     $('.footer__btn').on('click', function(){
       // $('.footer__btn').addClass('footer__btn--active');
     $('.footer__info-box').slideToggle('');
     $('.footer__btn-images').rotate({animateTo:180});
     });
   
-   
-    
-       
-
-
-
-
+// header-btn
     $('.header__btn').on('click', function(){
         $('.header__list'). slideToggle();
       });
-
+     
+     
    
       new WOW().init();
 
 
 
-	
+	// portfolio
     var mixer = mixitup('.portfolio__items',{
         animation: {
             effects: 'fade translateX(100%)',
@@ -70,3 +127,5 @@ $(function(){
     });	
     
 });
+
+// header__item-link--active
