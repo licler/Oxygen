afb3ca369960fe0a5dbbf52c5ead9e0a408dc6bf
licler <ute3@yandex.ru>
2022-03-24 20:47:09 +0300

    new-finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-// header
+// --header-------------------------------------------
   $(function() {
     let header = $('.header');
      
@@ -11,24 +11,9 @@ $(function(){
        header.removeClass('header__fixed');
       }
     });
-   });
+  });
 
-  
-
-//  let scrollOffset = 0,
-//      previousH = $('.previous').innerHeight(),
-//      header = $('.header');
-     
-
-//   $(window).on('scroll', function(){
-//      scrollOffset = $(this).scrollTop();
-//      if(scrollOffset >=previousH){
-//      header.addClass('header__fixed');
-//      } else {
-//       header.removeClass('header__fixed');
-//      }
-// });
-
+// ------------ клик по ссылкам----------------------
 $("[data-scroll]").on("click", function(event) {
   event.preventDefault();
 
@@ -39,93 +24,119 @@ $("[data-scroll]").on("click", function(event) {
   $("#nav a").removeClass("header__item-link--active");
   $this.addClass("header__item-link--active");
 
+  // закрытие меню после выбора ссылки/////////////////////////////////////////
+  if (menuBtn.classList.contains('active')){
+    document.body.classList.remove('_lock');
+    menuBtn.classList.remove('active');
+    menuList.classList.remove('active');
+   }
+   ///////////////////////////////////////////////////////////////////////
+
   $("html, body").animate({
       scrollTop:  blockOffset
   }, 500);
 });
+// --------------------------------------------------------
 
+
+
+//----------------------начало счетчик // --------------------------------------------------------
+	
+const counters = document.querySelectorAll('.numpage__item-num');
+
+counters.forEach((counter)  =>{
+  counter.innerHTML = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-value");
+    const c = +counter.innerText;
+
+    if(c < target) {
+      counter.innerText = c + 1;
+      setTimeout(updateCounter, 20);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCounter()
+})
+
+window.addEventListener('scroll', function(){
+document.getElementById('showScroll').innerHTML = pageYOffset + 'px';
+})
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//  начало счетчик
-	$('.numpage__item-num').each(function () {
-        $(this).prop('Counter', 0).animate({
-                Counter: $(this).data('value')
-            }, {
-            duration: 5000,
-            easing: 'swing',
-            step: function (now) {                      
-                $(this).text(Math.ceil(this.Counter));
-            }
-        });
-    });
-  
-  
+// -------------------------------------------------------- 
 
 
 // конец счетчик
 
 
-//  slider
+// --- slider----------------------------------
     $('.people__slider').slick({	
         arrows: true,
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/content/back.svg" alt="images"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/content/next.svg" alt="images"></button>',
         dots: true,
+        autoplaySpeed: 3000,
         responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              infinite: true,
+              dots: false,
+              arrows: false,
+              autoplay: true
+            }
+          },
             {
               breakpoint: 860,
               settings: {
                 infinite: true,
                 dots: false,
-                arrows: false
+                arrows: false,
+                autoplay: true
               }
             }
         ]
       });
 
+// --------------------------------------------------------
    
-// footer
+// --footer--------------------------------------
     $('.footer__btn').on('click', function(){
       // $('.footer__btn').addClass('footer__btn--active');
     $('.footer__info-box').slideToggle('');
     $('.footer__btn-images').rotate({animateTo:180});
     });
-  
-// header-btn
-    $('.header__btn').on('click', function(){
-        $('.header__list'). slideToggle();
-      });
-     
-     
+
+    
+//------------- меню бургер----------------------------------------
+    const menuBtn = document.querySelector('.header__btn');
+    const menuList = document.querySelector('.header__list');
+    
+    if(menuBtn) {
+      
+      menuBtn.addEventListener('click', function(event){
+         document.body.classList.toggle('_lock');
+         menuBtn.classList.toggle('active');
+         menuList.classList.toggle('active');
+      })
+    }
+    
+// -----------------------------------------------------------------
+
+
+ // --------------------------------------------------------    
    
       new WOW().init();
 
 
 
 	// portfolio
-    var mixer = mixitup('.portfolio__items',{
-        animation: {
-            effects: 'fade translateX(100%)',
-            reverseOut: true,
-            nudge: false 
-        }
+    var mixer = mixitup('.portfolio__items');	
+ // --------------------------------------------------------   
 
-    });	
-    
+
 });
 
 // header__item-link--active
